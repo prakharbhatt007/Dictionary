50b780fd9afcf2cc1df61e49d7f47c93413cfba2
prakharbhatt007 <prkhrbhtt@gmail.com>
2018-05-01 10:14:29 +0530

    seperating the correct value and wrong value into seperate dictionary

--- Dictionary-git.py
+++ Dictionary-git.py
@@ -1,4 +1,6 @@
 import random
+wrong={}
+right={}
 try:
     def check_word(y):
         if y in worddict:
@@ -9,12 +11,20 @@ try:
         for i in range(0,3):
                 word,me=random.choice(list(worddict.items()))
                 print(word)
+                
+                wrong_num=0
+                right_num=0
                 x=input()
-                print(me)
                 if(x.strip()==me.strip()):
                     print('correct')
+                    right_num=right_num+1
+                    right[word]=me
                 else:
                     print('Wrong Meaning Entered, the correct one is- '+me)
+                    wrong_num=wrong_num+1
+                    wrong[word]=me
+        print(right)
+        print(wrong)
     m=open('Meaning.txt','r+')
     worddict={}
     for i in m:

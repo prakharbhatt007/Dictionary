954b10c923935741fa57968116806cbfc6794b45
prakharbhatt007 <prkhrbhtt@gmail.com>
2018-05-11 22:02:50 +0530

    Removing some bugs

--- Projectictionary123321.py
+++ Projectictionary123321.py
@@ -1,54 +1,48 @@
 import random
+right={}
 def check_word(y):
     if y in worddict:
         print(y)
         print(worddict[y])
 def test():
+    if(len(worddict)<3):
+        print("Too less words in the database, please add some more")
+        return
     test={}
     print("Enter the meaning of following word")
     right_num=0
-    wrong_num=len(worddict)
-    print('ratio'+str(ratio))
     for i in range(0,3):
-            rr=random.randrange(1,100,1)
-            print(rr)
-            if (rr<=ratio):
+            if(random.randrange(0,100,1)<=ratio):
                 test=right
-            elif(rr>ratio):
+            else:
                 test=worddict
             word,me=random.choice(list(test.items()))
             print(word)            
             x=input()
             if(x.strip()==me.strip()):
                 print('correct')
-                right_num=right_num+1
-                wrong_num=wrong_num-1
-                worddict.pop(word)
+                worddict.pop(word)                
                 cright[word]=me
             else:
                 print('Wrong Meaning Entered, the correct one is- '+me)   
 try:
     m=open('Meaning.txt','r+')
+    c=open('Correct.txt','r+')
     worddict={}
-    worddict2=worddict
-    right={}
+    worddict2={}
     cright={}
     for i in m:
         (word,meaning)=i.split('-')
         worddict[word]=meaning
-    lenword=len(worddict)
-    c=open('Correct.txt','r+')
-    for j in c:
+    for k in c:
         try:
-            (cword,cmeaning)=j.split('-')
-            right[cword]=meaning
-        except ValueError as vrr:
+            (word,meaning)=k.split('-')
+            right[word]=meaning
+        except ValueError as vr:
             pass
-    for w in right:
-        if w in worddict:
-            worddict.pop(w)
     lenright=len(right)
-    ratio=round((lenright/lenword)*50,2)
+    lentotal=len(worddict)
+    ratio=round(lenright/lentotal)*50
     print("Enter F to find meaning or Enter T for Test")
     y=input()
     if(y=='F'):
@@ -56,14 +50,13 @@ try:
         check_word(input())
     if(y=='T'):
         test()
-    print('\n',file=c)
-    for k in cright:
-        if k not in right:
-            print(k+'-'+cright[k], file=c)
-        elif k in right:
+    for z in cright:
+        if z not in right:
+            print(cright+'-'+cright[z],file=m)
+        else:
             pass
 except IOError as err:
     print(str(err))
 finally:
-    c.close()
     m.close()
+    c.close()
